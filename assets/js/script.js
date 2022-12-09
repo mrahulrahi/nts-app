ed489d081fbe50d603201bd45828042103fac97f
FireLiquidator <mrahulrahi@gmail.com>
2022-12-10 01:45:38 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,18 +57,17 @@ class MyFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <footer class="page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="">
-                <img src="assets/imgs/logo.svg" alt="" class="logo">
+    <div class="container-footer">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57006.05879135503!2d80.9744585325986!3d26.748247508265365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfcccdbecff0f%3A0x305a04e692868308!2sJaneshwar%20Mishra%20Park!5e0!3m2!1sen!2sin!4v1670616737544!5m2!1sen!2sin" width="full" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="row-footer">
+            <div class="navbar-brand">
+                <img src="assets/imgs/logo.png" alt="" class="logo">
             </div>
-            <div class="">
-                <div class="socials">
-                    <a href="javascript:void(0)" class="font-weight-bold "><i class="fab fa-facebook"></i> 123,345</a>
-                    <a href="javascript:void(0)" class="font-weight-bold "><i class="fab fa-twitter"></i> 321,534</a>
-                    <a href="javascript:void(0)" class="font-weight-bold "><i class="fab fa-instagram"></i> 123,023</a>
-                    <a href="javascript:void(0)" class="font-weight-bold "><i class="fab fa-youtube"></i> 231,043</a>
-                </div>
+            <div class="socials">
+                <a href="javascript:void(0)"><i class="fab fa-twitter"></i> 321,534</a>
+                <a href="javascript:void(0)"><i class="fab fa-facebook"></i> 123,345</a>
+                <a href="javascript:void(0)"><i class="fab fa-youtube"></i> 231,043</a>
+                <a href="javascript:void(0)"><i class="fab fa-instagram"></i> 123,023</a>
             </div>  
         </div>
         <p class="border">&copy; <script>document.write(new Date().getFullYear())</script>, Imagine Group Created By <a href="https://www.devcrud.com" class="" target="_blank">mrahulrahi.</a>  All rights reserved </p> 
