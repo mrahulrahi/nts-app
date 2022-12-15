87ba1268f1aed7cc48bce3fbb2b61efd76dbe76a
FireLiquidator <mrahulrahi@gmail.com>
2022-12-15 22:23:16 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ class MyHeader extends HTMLElement {
             </ul>
             <div class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a href="tel:123-456-7890" class="btn"><i class="fa fa-phone"></i>Call Us +91XXXXXXXX</a>
+                    <a href="tel:123-456-7890" class="btn"><i class="fa fa-phone"></i> +91XXXXXXXX</a>
                 </li>
             </div>
         </div>
