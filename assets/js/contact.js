0f58e4a3e8518d9e5268d02e92cb0220acd1c9d5
FireLiquidator <mrahulrahi@gmail.com>
2023-01-24 21:19:50 +0530

    commit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,20 +1,20 @@
 function sendMail() {
   var params = {
     form_name: document.getElementById("form_name").value,
+    form_phone: document.getElementById("form_phone").value,
     form_email: document.getElementById("form_email").value,
-    form_subject: document.getElementById("form_subject").value,
     form_message: document.getElementById("form_message").value,
   };
 
   const serviceID = "service_o3uij72";
-  const templateID = "template_cihtuic";
+  const templateID = "template_n1gr88i";
 
   emailjs
     .send(serviceID, templateID, params)
     .then((res) => {
       (document.getElementById("form_name").value = ""),
+        (document.getElementById("form_phone").value = ""),
         (document.getElementById("form_email").value = ""),
-        (document.getElementById("form_subject").value = ""),
         (document.getElementById("form_message").value = ""),
         console.log(res);
       alert("Your message sent successfully");
