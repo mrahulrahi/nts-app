1f39d73e6e6f4240f56cced15becd36d6465effb
FireLiquidator <mrahulrahi@gmail.com>
2023-01-24 23:14:32 +0530

    commit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,23 +1,24 @@
-function sendMail() {
+(function () {
+  emailjs.init("i3xy6tBlSjmaB8XCJ");
+})();
+
+function sendEmail() {
+  let name = document.getElementById("form_name").value;
+  let phone = document.getElementById("form_phone").value;
+  let email = document.getElementById("form_email").value;
+  let message = document.getElementById("form_message").value;
+
   var params = {
-    form_name: document.getElementById("form_name").value,
-    form_phone: document.getElementById("form_phone").value,
-    form_email: document.getElementById("form_email").value,
-    form_message: document.getElementById("form_message").value,
+    form_name: name,
+    form_phone: phone,
+    form_email: email,
+    form_message: message,
   };
 
-  const serviceID = "service_o3uij72";
-  const templateID = "template_n1gr88i";
+  const serviceId = "service_gmail";
+  const templateId = "template_contact";
 
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      (document.getElementById("form_name").value = ""),
-        (document.getElementById("form_phone").value = ""),
-        (document.getElementById("form_email").value = ""),
-        (document.getElementById("form_message").value = ""),
-        console.log(res);
-      alert("Your message sent successfully");
-    })
-    .catch((err) => console.log(err));
+  emailjs.send(serviceId, templateId, params).then(function (res) {
+    alert("Sent Successfully");
+  });
 }
