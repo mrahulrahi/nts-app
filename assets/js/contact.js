dbcfae54b4fabca0c634caa312fcc6c971fa3c12
FireLiquidator <mrahulrahi@gmail.com>
2023-01-24 20:38:15 +0530

    commit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,9 +5,19 @@ function sendMail() {
     form_subject: document.getElementById("form_subject").value,
     form_message: document.getElementById("form_message").value,
   };
+
+  const serviceID = "service_o3uij72";
+  const templateID = "template_cihtuic";
+
   emailjs
-    .send("service_o3uij72", "template_fg4x4p7", params)
-    .then(function (res) {
-      alert("Success! " + res.status);
-    });
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      (document.getElementById("form_name").value = ""),
+        (document.getElementById("form_email").value = ""),
+        (document.getElementById("form_subject").value = ""),
+        (document.getElementById("form_message").value = ""),
+        console.log(res);
+      alert("Your message sent successfully");
+    })
+    .catch((err) => console.log(err));
 }
