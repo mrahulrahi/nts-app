cdb9c36ca31fc0a819a63c65d6847eb6440f2bff
FireLiquidator <mrahulrahi@gmail.com>
2023-01-25 09:35:37 +0530

    commit

---
+++ assets/js/book.js
@@ -0,0 +1,28 @@
+(function () {
+  emailjs.init("i3xy6tBlSjmaB8XCJ");
+})();
+
+function sendEmail() {
+  let name = document.getElementById("form_name").value;
+  let phone = document.getElementById("form_phone").value;
+  let email = document.getElementById("form_email").value;
+  let message = document.getElementById("form_message").value;
+  let email = document.getElementById("form_email").value;
+  let message = document.getElementById("form_message").value;
+
+  var params = {
+    form_name: name,
+    form_phone: phone,
+    form_email: email,
+    form_message: message,
+    form_email: email,
+    form_message: message,
+  };
+
+  const serviceId = "service_gmail";
+  const templateId = "template_book";
+
+  emailjs.send(serviceId, templateId, params).then(function (res) {
+    alert("Sent Successfully", window.location.reload());
+  });
+}
