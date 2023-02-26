2db33451adf2ef7adba8af12ff4fe3e6131a134b
FireLiquidator <mrahulrahi@gmail.com>
2023-02-26 09:38:42 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// header
+
 class MyHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
@@ -9,19 +11,24 @@ class MyHeader extends HTMLElement {
               <a class="navbar-title" href="#">
                   <h1>IMAGINE GROUP</h1>
               </a>
+              <div class="hamburger">
+              <span class="bar"></span>
+              <span class="bar"></span>
+              <span class="bar"></span>
+            </div>
 
-              <div class="socials">
-                  <a href="https://facebook.com/imaginegrouplko"><i class="fab fa-facebook"></i></a>
-                  <a href="javascript:void(0)"><i class="fab fa-twitter"></i></a>
-                  <a href="https://instagram.com/imaginegroupig"><i class="fab fa-instagram"></i></a>
-                  <a href="https://wa.me/+918881888339"><i class="fab fa-whatsapp"></i></a>
-              </div>
+             
           </div>
-      </nav>
-      
-      <nav class="navbar">
-          <div class="navbar-second">
-            <ul class="navbar-nav">                     
+          <div class="socials">
+          <a href="https://facebook.com/imaginegrouplko"><i class="fab fa-facebook"></i></a>
+          <a href="javascript:void(0)"><i class="fab fa-twitter"></i></a>
+          <a href="https://instagram.com/imaginegroupig"><i class="fab fa-instagram"></i></a>
+          <a href="https://wa.me/+918881888339"><i class="fab fa-whatsapp"></i></a>
+      </div>
+         
+
+        <div class="navbar-second">
+            <ul class="nav-menu">                     
                 <li class="nav-item">
                     <a class="nav-link" href="index.html">Home</a>
                 </li>
@@ -34,18 +41,38 @@ class MyHeader extends HTMLElement {
                 <li class="nav-item">
                     <a class="nav-link" href="index.html#contact">Contact Us</a>    
                 </li>
-            </ul>
-            <div class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a href="tel:888-188-8339" class="btn"><i class="fa fa-phone"></i> Call Us</a>
-                </li>
-            </div>
+                <a href="tel:888-188-8339" class="btn"><i class="fa fa-phone"></i> Call Us</a>
+            </li>
+            </ul>
+            
         </div>
+        
       </nav>
+      
       `;
   }
 }
 customElements.define("my-header", MyHeader);
+
+// hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+// footer
 
 class MyFooter extends HTMLElement {
   connectedCallback() {
