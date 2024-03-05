e7332a1a6a6ce67abd3333db8b7381180149b087
mrahulrahi <mrahulrahi@gmail.com>
2024-03-05 22:27:53 +0530

    testimonial card , about block, itblock changed

--- app/components/TestimonialCard/TestimonialCard.tsx
+++ app/components/TestimonialCard/TestimonialCard.tsx
@@ -15,7 +15,7 @@ const TestimonialCard = ({ card }: Card) => {
   const { name, title, body } = card;
 
   return (
-    <div className="testimonial-card-box w-100  d-flex flex-column bg-glass">
+    <div className="testimonial-card-box w-100 h-100 d-flex flex-column bg-glass">
       <h5 className="testimonial-card-title">{name}</h5>
       <h6 className="testimonial-card-subtitle">{title}</h6>
       <div className="testimonial-card-info mt-auto">{body}</div>
